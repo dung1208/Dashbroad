9d2cb1910eae56ab65831b6be59ee1d7d2d4ea2b
dung1208 <52040970+dung1208@users.noreply.github.com>
2019-10-15 21:43:20 +0700

    complete

--- modalCustomer.js
+++ modalCustomer.js
@@ -1,31 +1,79 @@
+    
+    // date picker
+$('#datepicker').datepicker({
+  uiLibrary: 'bootstrap4'
+});
+    
     // Get the modalAddCustomer
     var modalAddCustomer = document.getElementById("mymodalAddCustomer");
-    
     // Get the button that opens the modalAddCustomer
     var btnAddCustomer = document.getElementById("btnAddCustomer");
-    
     // When the user clicks the button, open the modalAddCustomer 
     btnAddCustomer.onclick = function() {
       modalAddCustomer.style.display = "block";
-    }
-    // When the user clicks anywhere outside of the modalAddCustomer, close it
-    window.onclick = function(event) {
-      if (event.target == modalAddCustomer) {
-        modalAddCustomer.style.display = "none";
+         // When the user clicks anywhere outside of the modalAddCustomer, close it
+      window.onclick = function(event) {
+        if (event.target == modalAddCustomer) {
+          modalAddCustomer.style.display = "none";
+        }
       }
     }
-
+ 
 
     var modalDelete = document.getElementById("mymodalDelete");
-   
- var btnDelete = document.getElementsByClassName("btnDelete");
- for( let i = 0; i < btnDelete.length; i++){
-        btnDelete[i].onclick = function() {
-             modalDelete.style.display = "block";
-             window.onclick = function(event) {
-                if (event.target == modalDelete) {
-                     modalDelete.style.display = "none";
+    var btnDelete = document.getElementsByClassName("btnDelete");
+    for( let i = 0; i < btnDelete.length; i++){
+           btnDelete[i].onclick = function() {
+                modalDelete.style.display = "block";
+                window.onclick = function(event) {
+                   if (event.target == modalDelete) {
+                        modalDelete.style.display = "none";
+                       }
                     }
-                 }
-            }
+               }
+       }
+
+
+ var modalViewProfile = document.getElementById("mymodalViewProfile");
+ var btnViewProfile = document.getElementsByClassName("btnViewProfile");
+ var closeViewProfile = document.getElementsByClassName('closeViewProfile')[0];
+ for( let i = 0; i < btnViewProfile.length; i++){
+  btnViewProfile[i].onclick = function() {
+     modalViewProfile.style.display = "block";
+     window.onclick = function(event) {
+      if (event.target == modalViewProfile) {
+           modalViewProfile.style.display = "none";
+          }
+        }
+        closeViewProfile.onclick = () => {
+          modalViewProfile.style.display = "none";
+        }
     }
+  }
+
+
+  var modalEditCustomer = document.getElementById("mymodalEditCustomer");
+  var btnEditCustomer = document.getElementsByClassName("btnEditCustomer");
+  for( let i = 0; i < btnEditCustomer.length; i++){
+  btnEditCustomer[i].onclick = function() {
+    modalEditCustomer.style.display = "block";
+    window.onclick = function(event) {
+      if (event.target == modalEditCustomer) {
+        modalEditCustomer.style.display = "none";
+      }
+    }
+  }
+}
+
+var modalGenerateLicense = document.getElementById("mymodalGenerateLicense");
+var btnGenerateLicense = document.getElementsByClassName("btnGenerateLicense");
+for( let i = 0; i < btnGenerateLicense.length; i++){
+btnGenerateLicense[i].onclick = function() {
+  modalGenerateLicense.style.display = "block";
+  window.onclick = function(event) {
+    if (event.target == modalGenerateLicense) {
+      modalGenerateLicense.style.display = "none";
+    }
+  }
+}
+}
--- modalLicense.js
+++ modalLicense.js
@@ -6,12 +6,10 @@ $('#datepicker').datepicker({
 
 
 var modalAddLicense = document.getElementById("mymodalAddLicense");
-   
 var btnAddLicense = document.getElementById("btnAddLicense");
 btnAddLicense.onclick = function() {
      modalAddLicense.style.display = "block";
      window.onclick = function(event) {
-        console.log(event);
       if (event.target == modalAddLicense) {
            modalAddLicense.style.display = "none";
           }
@@ -21,7 +19,6 @@ btnAddLicense.onclick = function() {
 
 
 var modalDelete = document.getElementById("mymodalDelete");
-   
  var btnDelete = document.getElementsByClassName("btnDelete");
  for( let i = 0; i < btnDelete.length; i++){
         btnDelete[i].onclick = function() {
@@ -34,4 +31,33 @@ var modalDelete = document.getElementById("mymodalDelete");
             }
     }
 
-    
+
+    var modalViewLicenses = document.getElementById("mymodalViewLicenses");
+    var btnViewLicenses = document.getElementsByClassName("btnViewLicenses");
+    var closeViewLicenses = document.getElementsByClassName('closeViewLicenses')[0];
+    for( let i = 0; i < btnViewLicenses.length; i++){
+   btnViewLicenses[i].onclick = function() {
+        modalViewLicenses.style.display = "block";
+        window.onclick = function(event) {
+         if (event.target == modalViewLicenses) {
+              modalViewLicenses.style.display = "none";
+             }
+           }
+           closeViewLicenses.onclick = () => {
+             modalViewLicenses.style.display = "none";
+           }
+       }
+     }
+
+     var modalDisable = document.getElementById("mymodalDisable");
+ var btnDisable = document.getElementsByClassName("btnDisable");
+ for( let i = 0; i < btnDisable.length; i++){
+        btnDisable[i].onclick = function() {
+             modalDisable.style.display = "block";
+             window.onclick = function(event) {
+                if (event.target == modalDisable) {
+                     modalDisable.style.display = "none";
+                    }
+                 }
+            }
+    }
