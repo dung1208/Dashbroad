72832ad9662230bd3380201be7f43c4623a10a45
dung1208 <52040970+dung1208@users.noreply.github.com>
2019-10-16 15:20:51 +0700

    1

--- modalLicense.js
+++ modalLicense.js
@@ -35,8 +35,14 @@ var modalDelete = document.getElementById("mymodalDelete");
     var modalViewLicenses = document.getElementById("mymodalViewLicenses");
     var btnViewLicenses = document.getElementsByClassName("btnViewLicenses");
     var closeViewLicenses = document.getElementsByClassName('closeViewLicenses')[0];
+    var customerModalViewLicenses = document.getElementsByClassName('customerModalViewLicenses');
+    var customerDisplayModalViewLicenses = document.getElementsByClassName('customerDisplayModalViewLicenses')[0];
+    var statusLicenseModalViewLicenses = document.getElementsByClassName('statusLicenseModalViewLicenses');
+    var statusDisplayModalViewLicenses = document.getElementsByClassName('statusDisplayModalViewLicenses')[0];
     for( let i = 0; i < btnViewLicenses.length; i++){
    btnViewLicenses[i].onclick = function() {
+     statusDisplayModalViewLicenses.innerText = statusLicenseModalViewLicenses[i].textContent;  
+     customerDisplayModalViewLicenses.innerText = customerModalViewLicenses[i].textContent;
         modalViewLicenses.style.display = "block";
         window.onclick = function(event) {
          if (event.target == modalViewLicenses) {
